9b42e20fccb1c75135952d21bc5f3058eafa04d4
ElenaV <helenavlasova39@gmail.com>
2020-08-26 14:10:32 +0300

    New button for saving keywords instead of firing save event for each input

--- design/click-handlers.js
+++ design/click-handlers.js
@@ -1,31 +1,5 @@
 window.onload = function () {
-    /*var sectionProgressHeader = document.getElementsByClassName("node-property__header--progress")[0];
-    if(sectionProgressHeader) {
-        var sectionProgressContent = document.getElementsByClassName("node-property__content--progress")[0];
-        if(sectionProgressContent) {
-            sectionProgressHeader.onclick = function() {
-                sectionProgressContent.classList.toggle("hidden");
-            }
-        }
-    }
-
-    var sectionCountHeader = document.getElementsByClassName("node-property__header--count")[0];
-    if(sectionCountHeader) {
-        var sectionCountContent = document.getElementsByClassName("node-property__content--count")[0];
-        sectionCountHeader.onclick = function() {
-            sectionCountContent.classList.toggle("hidden");
-        }
-    }
-
-    var sectionKeywordsHeader = document.getElementsByClassName("node-property__header--keyword")[0];
-    if(sectionKeywordsHeader) {
-        var sectionKeywordsContent = document.getElementsByClassName("node-property__content--keyword")[0];
-        sectionKeywordsHeader.onclick = function() {
-            sectionKeywordsContent.classList.toggle("hidden");
-        }
-    }*/
-
-    var keywordButton = document.getElementsByClassName("keyword__button")[0];
+    var keywordButton = document.getElementsByClassName("keyword__button--add")[0];
     var keywordButtonInput = keywordButton.querySelectorAll("button")[0];
     if(keywordButtonInput) {
         keywordButtonInput.onclick = addKeywordsRow;
@@ -40,7 +14,7 @@ function addKeywordsRow() {
 
     newRowInputs.forEach(function (rowInput) {
         rowInput.value = '';
-        rowInput.addEventListener('change', saveChanges);
+        //rowInput.addEventListener('change', saveChanges);
     });
 
     keywordsTable.appendChild(newRow);
