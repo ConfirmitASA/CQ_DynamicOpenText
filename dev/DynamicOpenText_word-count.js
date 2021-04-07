3ed1ce8d27596ccaac0d4bfdbb893cc47b176061
AnnaGo-cf <anna.goncharova@confirmit.com>
2021-04-07 16:01:03 +0300

    TQA-6707: resolved an issue with newline breaks prevent counting the first word of the line

--- dev/DynamicOpenText_word-count.js
+++ dev/DynamicOpenText_word-count.js
@@ -37,9 +37,10 @@ export default class WordCount {
     }
 
     getStringWithSingleSpacedWords = (str) => {
-        str = str.replace(/(^\s*)|(\s*$)/gi,"");
-        str = str.replace(/[ ]{2,}/gi," ");
-        str = str.replace(/\n /,"\n");
+        str = str.replace(/(^\s*)|(\s*$)/gi,""); //remove the start and end spaces
+        str = str.replace(/[ ]{2,}/gi," "); //reduce multiple spaces to a single space
+        str = str.replace(/ \n|\n | \n /g,"\n"); //exclude spaces near newlines
+        str = str.replace(/\n|\r/g, " "); //replace newlines and carriage returns with spaces
 
         return str;
     }
